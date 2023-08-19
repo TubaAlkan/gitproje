3df648aa99fdf08d1fea50b499743b8b986ca6a7
Tuba Alkan <t.alkan.k@gmail.com>
2023-08-19 21:43:25 +0300

    v4

--- src/main/java/class01.java
+++ src/main/java/class01.java
@@ -12,9 +12,25 @@ public class class01 {
         int z=5;
         System.out.println(x+y+z);
 
+        //versiyon 3
+        System.out.println("versiyon 3");
+
+        //git init ile terminalden kaydediyoruz.
+        //git status statüyü kontrol ettiriyor
+       //git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani.
+       //git commit -m" başlık gelecek buraya "
+       //git log ile versiyon görünür bilgiler
+       //cls ile alan temizleniyor kodlar yani
+       //git commit --amend -m""  isim değiştirme (hangi versiyondaysan)
+        // klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
+       //git log --oneline ile versiyonları tek satırlar halinde gösteriyor.
 
 
+        //working space ilk
 
+        //versiyon 4
 
+        System.out.println("versiyon 4");
     }
+
 }
