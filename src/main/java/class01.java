ea6bcea1e634456030d32f66c2a980977d0936f8
Tuba Alkan <t.alkan.k@gmail.com>
2023-08-19 20:30:09 +0300

    versiyon1

---
+++ src/main/java/class01.java
@@ -0,0 +1,16 @@
+public class class01 {
+    public static void main(String[] args) {
+        //version 1 icin
+
+        int x=1;
+        int y=2;
+
+        System.out.println(x+y);
+
+
+
+
+
+
+    }
+}
